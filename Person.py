505d07d73476a7be41491722ba8d35e80ed34914
Urge2purge <you@example.com>
2024-04-07 15:56:44 +0530

    Signed-off-by: Urge2purge <you@example.com>

---
+++ Person.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, loan_id, month, loan_status):
+        self.loan_id = loan_id
+        self.month = month
+        self.loan_status = loan_status
+
+    def display(self):
+        print(self.loan_id,",",self.month,",",self.loan_status)
+
+    def talk():
+        pass
+
+if __name__ == "__main__":
+    p = Person("001","20240131","3")
+    p.display()
+    p.talk()
+    
